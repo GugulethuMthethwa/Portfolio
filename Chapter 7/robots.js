1544d3b89ef4cf158115d18132de10d50567a409
GugulethuMthethwa <gugulethumthethwa2508@gmail.com>
2024-12-23 21:49:57 +0200

    Update robots.js

--- Chapter 7/robots.js
+++ Chapter 7/robots.js
@@ -1,7 +1,7 @@
 document.getElementById("lefteye").style.backgroundColor = "purple";
 document.getElementById("head").style.transform = "rotate(15deg)";
 // Put a 2‐pixel‐wide, solid black border around his body.
-document.getElementById("body").style.border = "2px blacksolid";
+document.getElementById("body").style.border = "2px black solid";
 // Round the corners of his mouth.
 document.getElementById("mouth").style.borderRadius = "4px";
 // Put yellow dots around his right eye.
@@ -13,35 +13,36 @@ document.getElementById("body").style.color = "#FF0000";
 // Give Douglas hair.
 document.getElementById("head").style.borderTop = "6px black solid";
 
-// var rightEye = document.getElementById("righteye");
-// rightEye.addEventListener("click", moveUpDown);
+var rightEye = document.getElementById("righteye");
+rightEye.addEventListener("click", moveUpDown);
 
-// function moveUpDown(e) {
-//     var robotPart = e.target;
-//     var top = 0;
-//     var id = setInterval(frame, 10) // draw every 10ms
+function moveUpDown(e) {
+    var robotPart = e.target;
+    var top = 0;
+    var id = setInterval(frame, 10) // draw every 10ms
 
-//     function frame() {
-//         robotPart.style.top = top + '%';
-//         top++;
-//         if (top === 20) {
-//             clearInterval(id);
-//         }
-//     }
-// }
+    function frame() {
+        robotPart.style.top = top + '%';
+        top++;
+        if (top === 20) {
+            clearInterval(id);
+        }
+    }
+}
 
-// var leftArm = document.getElementById("leftarm");
-// leftArm.addEventListener("click", moveRightLeft);
+var leftArm = document.getElementById("leftarm");
+leftArm.addEventListener("click", moveRightLeft);
 
-// function moveRightLeft(e) {
-//     var robotPart = e.target;
-//     var left = 0;
-//     var id = setInterval(frame, 10) // draw every 10ms
-//     function frame() {
-//         robotPart.style.left = left + '%';
-//         left++;
-//         if (left === 70) {
-//             clearInterval(id);
-//         }
-//     }
-// }
+function moveRightLeft(e) {
+    var robotPart = e.target;
+    var left = 0;
+    var id = setInterval(frame, 10) // draw every 10ms
+    
+    function frame() {
+        robotPart.style.left = left + '%';
+        left++;
+        if (left === 70) {
+            clearInterval(id);
+        }
+    }
+}
